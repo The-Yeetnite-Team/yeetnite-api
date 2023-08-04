068ad2fbbb092ce399c0b386e72415c3dcdc191e
Antonios Papadakis <bdpaboi123@gmail.com>
2023-08-04 13:30:06 +0300

    Add Configuration File Template

---
+++ api-config.template.php
@@ -0,0 +1,28 @@
+<?php
+define('DB_HOST', 'abcd');
+define('DB_USERNAME', 'abcd');
+define('DB_PASSWORD', 'abcd');
+define('DB_NAME', 'abcd');
+define('MEMCACHED_HOST', 'abcd');
+define('MEMCACHED_PORT', 11211);
+define('TIGASE_API_KEY', 'abcd');
+define('TIGASE_HTTP_AUTHORIZATION', 'abcd');
+define('DEFAULT_MEMCACHED_KEYS', array(
+    'friend_request_error_self_accept' => '{"success":false,"reason":"You can\'t accept your own friend request"}',
+    'content_api_pages_fortnite_game' => 'file|static/content_api_pages_fortnite_game.json',
+    'fortnite_api_calendar_v1_timeline' => 'file|static/fortnite_api_calendar_v1_timeline.json',
+    'fortnite_api_cloudstorage_system' => 'file|static/fortnite_api_cloudstorage_system.json',
+    'fortnite_api_cloudstorage_transports' => 'file|static/fortnite_api_cloudstorage_transports.json',
+    'fortnite_api_game_v2_profile_athena' => 'file|static/fortnite_api_game_v2_profile_athena.json',
+    'fortnite_api_game_v2_profile_collection_book_people0' => 'file|static/.json',
+    'fortnite_api_game_v2_profile_collection_book_schematics0' => 'file|static/fortnite_api_game_v2_profile_collection_book_schematics0.json',
+    'fortnite_api_game_v2_profile_common_core' => 'file|static/fortnite_api_game_v2_profile_common_core.json',
+    'fortnite_api_game_v2_profile_common_public' => 'file|static/fortnite_api_game_v2_profile_common_public.json',
+    'fortnite_api_game_v2_profile_expeditions_campaign' => 'file|static/fortnite_api_game_v2_profile_expeditions_campaign.json',
+    'fortnite_api_game_v2_profile_metadata' => 'file|static/fortnite_api_game_v2_profile_metadata.json',
+    'fortnite_api_game_v2_profile_profile0' => 'file|static/fortnite_api_game_v2_profile_profile0.json',
+    'fortnite_api_game_v2_profile_theater0' => 'file|static/fortnite_api_game_v2_profile_theater0.json',
+    'fortnite_api_game_v2_world_info' => 'file|static/fortnite_api_game_v2_world_info.json',
+    'fortnite_api_storefront_v2_catalog' => 'file|static/fortnite_api_storefront_v2_catalog.json',
+    'fortnite_api_storefront_v2_keychain' => 'file|static/fortnite_api_storefront_v2_keychain.json',
+));
