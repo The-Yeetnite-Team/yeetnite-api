dc7fb642cb89856a777f522bfe4579a3e8d8baf5
Antonios Papadakis <bdpaboi123@gmail.com>
2023-08-16 19:06:22 -0500

    Max Yeetnite Client ID Naming Consistent

--- api/v3/account/api/oauth/verify.php
+++ api/v3/account/api/oauth/verify.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 echo json_encode(
     array(
         'access_token' => substr($_SERVER['HTTP_AUTHORIZATION'], 7),
-        'client_id' => 'yeetniteclientlol',
+        'client_id' => 'yeetnite-client',
         'client_service' => 'prod-fn',
         'expires_at' => '9999-12-02T01:12:00Z',
         'expires_in' => 28800,
