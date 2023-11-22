8c5201ef6a2014d3fd2dead61efdfc1ed74d7b8f
Antonios Papadakis <bdpaboi123@gmail.com>
2023-11-22 02:38:50 -0500

    Update docker-compose To Use Ports > 1024

--- tests/UserAuthenticationTest.php
+++ tests/UserAuthenticationTest.php
@@ -7,7 +7,7 @@ class UserAuthenticationTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://localhost/api/v3/']);
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://localhost:1880/api/v3/']);
     }
 
     protected function tearDown(): void
@@ -15,12 +15,12 @@ class UserAuthenticationTest extends TestCase
         $this->http = null;
     }
 
-    public function testLogin()
+    public function testLauncherLogin()
     {
         $userInfo = $this->http->get('launcher_login.php?username=testUser&password=Antonios12!');
+        $this->assertEquals(200, $userInfo->getStatusCode());
         $body = json_decode($userInfo->getBody()->getContents(), true);
 
-        $this->assertEquals(200, $userInfo->getStatusCode());
         $this->assertTrue($body['success']);
         
         $this->assertEquals('testUser', $body['username']);
